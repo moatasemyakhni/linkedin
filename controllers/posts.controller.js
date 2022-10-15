c3cbc199ffea1eb15f2a8533430cf25a39cb6194
= <motasem.yakhni@gmail.com>
2022-10-15 17:05:57 +0300

    Backend => Post: fix createPost function

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,13 +1,22 @@
 const Post = require('../models/Post');
 
-const createPost = (req, res) => {
+const createPost = async (req, res) => {
     const company_id = req.body._id;
     const content = req.body.content;
-
-    const post = new Post();
-    post.company_id = company_id;
-    post.content = content;
-    post.save();
+    try {
+        const post = new Post();
+        post.company_id = company_id;
+        post.content = content;
+        await post.save();
+        res.status(200).json({
+            post: post
+        });
+    }catch(err) {
+        res.json({
+            "message": "insert failed",
+            "error": err.message
+        });
+    }
 }
 
 module.exports = {
