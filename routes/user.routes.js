5714ea3ef7b222e953a3365d424796feb9cea415
= <motasem.yakhni@gmail.com>
2022-10-15 13:01:06 +0300

    Backend => User: create route to make user follow company

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-const {login, signup, getAllUsers, updateProfilePicture} = require('../controllers/users.controller');
+const {login, signup, getAllUsers, updateProfilePicture, followCompany} = require('../controllers/users.controller');
 const router = Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
 router.get('/', getAllUsers);
 router.patch('/', updateProfilePicture);
+router.patch('/follow_company', followCompany);
 module.exports = router;
