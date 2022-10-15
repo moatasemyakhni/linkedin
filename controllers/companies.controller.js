ee15a61d08388074e5590d3d19a67de17d2deab2
= <motasem.yakhni@gmail.com>
2022-10-15 15:36:13 +0300

    Backend => Company: create token generator function

--- controllers/companies.controller.js
+++ controllers/companies.controller.js
@@ -3,3 +3,23 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const user_type = 'company';
+
+const createToken = (company) => {
+    const token = jwt.sign({
+        email: company.email,
+        name: `${company.first_name} ${company.last_name}`,
+        profile: company.profile,
+        headline: company.headline,
+        country: company.country,
+        city: company.city,
+        phone: company.phone,
+        user_type: user_type,
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+            expiresIn: '10h',
+        }
+    );
+
+    return token;
+}
