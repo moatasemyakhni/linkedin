a28dfb668558d7b6327ff4ba8d1fa649f183b660
= <motasem.yakhni@gmail.com>
2022-10-15 16:15:18 +0300

    Backend => create company url route

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.use(express.json());
 
 
 const userRoutes = require('./routes/user.routes');
+const companyRoutes = require('./routes/company.routes');
 app.use('/users', userRoutes);
-
+app.use('/company', companyRoutes);
 app.listen(process.env.PORT, (err) => {
     if(err) {
         console.error(err);
