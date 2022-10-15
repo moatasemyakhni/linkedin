171e8aaa7655731726950b6f8868b724e7d704af
= <motasem.yakhni@gmail.com>
2022-10-15 16:07:00 +0300

    Backend => POST: create post creator

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,4 +1,11 @@
 const Post = require('../models/Post');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
+const createPost = (req, res) => {
+    const company_id = req.body._id;
+    const content = req.body.content;
+
+    const post = new Post();
+    post.company_id = company_id;
+    post.content = content;
+    post.save();
+}
