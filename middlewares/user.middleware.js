b2d6cb0ba9cc64db741e414f1c386b7d3985c9c2
= <motasem.yakhni@gmail.com>
2022-10-15 13:46:31 +0300

    Backend => fix users middleware

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -7,7 +7,7 @@ const userAuthMiddleware = async (req, res, next) => {
     if(!token) return res.status(401).json({message: "Unauthorized"});
     try{
         const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET_KEY);
-        const user = await User.findOne({email: decoded.email}).lean()
+        const user = await User.findOne({email: decoded.email}).lean();
         next()
 
     }catch(err){
