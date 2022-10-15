f7dd5d0e8e6c1779d09a27eeec119a9a97ed86e5
= <motasem.yakhni@gmail.com>
2022-10-15 16:13:22 +0300

    Backend => COMPANY:  create login and signup routes

--- routes/company.routes.js
+++ routes/company.routes.js
@@ -1,2 +1,9 @@
 const {Router} = require('express');
 const {login, signup} = require('../controllers/companies.controller');
+
+const router = Router();
+
+router.post('/login', login);
+router.post('/signup', signup);
+
+module.exports = router;
