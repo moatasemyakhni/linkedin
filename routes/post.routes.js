bc05470732393801b671b51769e782f391cb6262
= <motasem.yakhni@gmail.com>
2022-10-15 17:26:32 +0300

    Backend => add router for apply to job function

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 
-const {createPost} = require('../controllers/posts.controller');
+const {createPost, applyToPost} = require('../controllers/posts.controller');
 
 const router = Router();
 
 router.post('/', createPost);
+router.post('/apply_for_job', applyToPost);
 
 module.exports = router;
