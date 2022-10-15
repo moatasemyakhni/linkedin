4c6cda754e6cbf60c1c65ca0aaa30e8f34127dee
= <motasem.yakhni@gmail.com>
2022-10-15 08:54:05 +0300

    Backend => User: create login controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,4 +2,33 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const user_type = 'user';
 
+const login = async (req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user) return res.status(404).json({message: 'Invalid Credentials'});
+
+    const checkPassword = bcrypt.compare(password, user.password);
+    if(!checkPassword) return res.status(404).json({message: 'Invalid Credentials'});
+
+    const token = jwt.sign({
+        email: user.email,
+        name: `${user.first_name} ${user.last_name}`,
+        profile: user.profile,
+        headline: user.headline,
+        country: user.country,
+        city: user.city,
+        phone: user.phone,
+        user_type: user_type,
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+            expiresIn: '10h',
+        }
+    );
+
+    res.status(400).json(token);
+
+}
