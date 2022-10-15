05117fcb6cced7259e9e33b1d30813949cd66653
= <motasem.yakhni@gmail.com>
2022-10-15 18:26:54 +0300

    Backend => remove middleware from login and signup company routes

--- routes/company.routes.js
+++ routes/company.routes.js
@@ -1,10 +1,9 @@
 const {Router} = require('express');
 const {login, signup} = require('../controllers/companies.controller');
-const companyAuthMiddleware = require('../middlewares/company.middleware');
 
 const router = Router();
 
-router.post('/login', companyAuthMiddleware, login);
-router.post('/signup', companyAuthMiddleware, signup);
+router.post('/login', login);
+router.post('/signup', signup);
 
 module.exports = router;
