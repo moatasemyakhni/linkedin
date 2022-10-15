b68539ebcb66498015dc0e8eab8090dde257ebb1
= <motasem.yakhni@gmail.com>
2022-10-15 17:43:37 +0300

    Backend => change post to patch in post router

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,6 +5,6 @@ const {createPost, applyToPost} = require('../controllers/posts.controller');
 const router = Router();
 
 router.post('/', createPost);
-router.post('/apply_for_job', applyToPost);
+router.patch('/apply_for_job', applyToPost);
 
 module.exports = router;
