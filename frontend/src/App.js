d04d3c80a2bf081e41692c10667bd50441da585a
= <motasem.yakhni@gmail.com>
2022-10-16 09:02:52 +0300

    Frontend => create navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo2 from './assets/images/logo/nav-logo.svg';
 import './App.css';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo2} alt="logo" />
+        {/* <img src={logo2} alt="logo" /> */}
+        <Navbar />
     </div>
   );
 }
